208e8e1e43fe4781ec57bfe77e777e1d5d53a253
me-aditya <adityarathi1212@gmail.com>
2021-05-01 04:10:13 +0530

    commented console print statements App ready for production

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -113,7 +113,7 @@ class _InputPageState extends State<InputPage> {
                       max: 250.0,
                       value: height.toDouble(),
                       onChanged: (double changedValue) {
-                        print(changedValue);
+                        //print(changedValue);
                         setState(() {
                           height = changedValue.round();
                         });
